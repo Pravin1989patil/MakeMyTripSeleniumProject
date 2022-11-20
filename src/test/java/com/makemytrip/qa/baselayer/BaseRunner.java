89ed7e607639b0116d4100a961c130c20f156b8b
hp <hp@DESKTOP-TLOIOJ0>
2022-11-20 17:02:51 +0530

    Added Homepage class and added code to retrive pagetitle

--- src/test/java/com/makemytrip/qa/baselayer/BaseRunner.java
+++ src/test/java/com/makemytrip/qa/baselayer/BaseRunner.java
@@ -1,19 +1,19 @@
 package com.makemytrip.qa.baselayer;
-
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class BaseRunner {
 
 	public static WebDriver driver;
+
 	public static void main(String[] args) {
-		
-		driver= new ChromeDriver();
+
+		driver = new ChromeDriver();
 		driver.get("https://www.makemytrip.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.getCurrentUrl();
+		driver.getTitle();
 
 	}
 
---
+++ src/test/java/com/makemytrip/qa/pagelayer/HomePage.java
@@ -0,0 +1,10 @@
+package com.makemytrip.qa.pagelayer;
+
+public class HomePage {
+
+	
+	public HomePage()
+	{
+		System.out.println("I am in home page");
+	}
+}
